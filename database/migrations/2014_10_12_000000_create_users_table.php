c0a5363c2a681430e1f64b597c331fb1b13520e1
Millat Hussain <20220071@Micromac.com>
2024-07-15 19:25:10 +0600

    Users Table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,12 +13,24 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
+            $table->string('phone')->unique();
+            $table->string('user_name')->unique();
+            $table->string('img_url')->unique();
+            $table->tinyInteger('admin_status')->default(0)->comment("1=Admin 0=Normal User");
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('confrim_password');
+            $table->integer('created_by');
+            $table->date('created_at');
+            $table->integer('update_by');
+            $table->date('update_at');
+            $table->integer('delete_by');
+            $table->date('delete_at');
+            $table->tinyInteger('status')->default(1)->comment("1=Active");
             $table->rememberToken();
-            $table->timestamps();
         });
     }
 
